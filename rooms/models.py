a82d669b4a31466fefb351b46722cd0ef367671a
Will Refvem <wbrefvem@gmail.com>
2015-06-09 13:54:35 -0400

    Added room and building models

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Building(models.Model):
+    name = models.CharField(max_length=256)
+    address = models.CharField(max_length=256)
+
+
+class Room(models.Model):
+    building = models.ForeignKey(Building)
+    room_number = models.CharField(max_length=256)
+    room_type = models.CharField(max_length=256)
+    manager = models.CharField(max_length=256)
+    general_usage = models.BooleanField()
+    seating = models.IntegerField()
+    display = models.BooleanField()
+    phone = models.BooleanField()
+    network = models.BooleanField()
+    usage_restrictions = models.CharField(max_length=256)
+    has_usage_restrictions = models.BooleanField()
