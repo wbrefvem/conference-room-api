0fe4eff37464cc089723dd01e241896a09169b16
Will Refvem <wbrefvem@gmail.com>
2015-10-08 15:42:35 -0400

    DB fix

--- conferenceroom/settings.py
+++ conferenceroom/settings.py
@@ -59,12 +59,16 @@ WSGI_APPLICATION = 'conferenceroom.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'conferenceroom',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'conferenceroom',
-    #     'USER': 'postgres',
-    #     'HOST': 'localhost',
-    #     'PORT': 5432,
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': 'db.sqlite3',
     # }
 }
 
@@ -82,7 +86,7 @@ USE_L10N = True
 USE_TZ = True
 
 # Parse database configuration from $DATABASE_URL
-DATABASES['default'] = dj_database_url.config()
+#DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
